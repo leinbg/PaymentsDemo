dd8d28703b5aa5534a6175f1a81beae1e5d03957
leinbg <leiw4github@gmail.com>
2016-11-20 22:37:08 +0100

    plan table migration and seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(ProductTableSeeder::class);
+        $this->call(PlanTableSeeder::class);
     }
 }
