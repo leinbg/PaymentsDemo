110e5d4b5ba950a899faa5bcb870f95c05834879
leinbg <leiw4github@gmail.com>
2016-11-20 19:37:19 +0100

    add comment

--- app/Product.php
+++ app/Product.php
@@ -4,8 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Product
+ *
+ * @package App
+ */
 class Product extends Model
 {
+
+    /**
+     * @var array
+     */
     protected $casts = [
         'price' => 'integer'
     ];
