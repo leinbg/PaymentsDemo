ed784ecaa3eae7c8b5f63ea3e6f6c877ff207ff7
leinbg <leiw4github@gmail.com>
2016-11-20 14:46:43 +0100

    html Code delete and add Vue Template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,44 +4,19 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script type="text/javascript">
+            var myApp = {
+                "csrfToken" : "{{ csrf_token() }}",
+                "stripeKey" : "{{ config('services.stripe.key') }}"
+            };
+        </script>
     </head>
     <body>
-        <form id="checkout-form" action="/purchase" method="POST">
-            {{ csrf_field() }}
-            <input type="hidden" id="stripeToken" name="stripeToken">
-            <input type="hidden" id="stripeEmail" name="stripeEmail">
-            <button class="purchase-shoe">buy this shoe</button>
-        </form>
+        <div id="app">
+            <checkout-form></checkout-form>
+        </div>
 
         <script src="https://checkout.stripe.com/checkout.js"></script>
-        <script>
-            var stripe = StripeCheckout.configure({
-                key: "{{ config('services.stripe.key') }}",
-                image: "https://stripe.com/img/documentation/checkout/marketplace.png",
-                locale: "auto",
-                token: function (token) {
-                    document.querySelector('#stripeToken').value = token.id;
-                    document.querySelector('#stripeEmail').value = token.stripeEmail;
-
-                    document.querySelector('#checkout-form').submit();
-                }
-            });
-
-            document.querySelector('.purchase-shoe').addEventListener('click', function (e) {
-                e.preventDefault();
-                stripe.open({
-                    name: "Buy This Shoe",
-                    description: "super christmas discount for this shoe",
-                    zipCode: true,
-                    currency: "eur",
-                    amount: 2000
-                });
-            });
-
-            // Close Checkout on page navigation:
-            window.addEventListener('popstate', function() {
-                stripe.close();
-            });
-        </script>
+        <script src="/js/app.js"></script>
     </body>
 </html>
