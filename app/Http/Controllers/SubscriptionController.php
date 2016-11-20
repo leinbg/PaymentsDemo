5aff74f7adcf50c30fc8e730893bd1e70c20b7a3
leinbg <leiw4github@gmail.com>
2016-11-20 22:37:45 +0100

    change purchase controller to subscription controller

---
+++ app/Http/Controllers/SubscriptionController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Plan;
+use Illuminate\Http\Request;
+use Stripe\{Customer, Stripe};
+
+/**
+ * Class SubscriptionController
+ *
+ * @package App\Http\Controllers
+ */
+class SubscriptionController extends Controller
+{
+
+    /**
+     * @param Request $request
+     */
+    public function store(Request $request)
+    {
+        Stripe::setApiKey(config('services.stripe.secret'));
+
+        $plan = Plan::findOrFail($request->plan);
+
+        try {
+            $customer = Customer::create(array(
+                'email' => $request->stripeEmail,
+                'source'  => $request->stripeToken,
+                'plan' => $plan->name,
+            ));
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 422);
+        }
+    }
+}
