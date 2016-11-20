ac801fc23acf36ced51cbb8449adf1eb33409ccc
leinbg <leiw4github@gmail.com>
2016-11-20 23:34:57 +0100

    stripe in user model fillable

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'stripe_id', 'stripe_active', 'stripe_subscription_end_at'
     ];
 
     /**
