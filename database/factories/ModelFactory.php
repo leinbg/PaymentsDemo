662d87475417d457780c5f449133736ac3f74006
leinbg <leiw4github@gmail.com>
2016-11-20 15:15:46 +0100

    migration and seeding database

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,11 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Product::class, function (Faker\Generator $faker) {
+    return [
+        'title' => $faker->name,
+        'description' => $faker->sentence,
+        'price' => $faker->numberBetween(1000, 10000),
+    ];
+});
