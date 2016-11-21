91ea21de7f5fc7acb981790d9216fa4bea553d6f
leinbg <leiw4github@gmail.com>
2016-11-21 23:36:50 +0100

    outsource stripe register in form request class

---
+++ app/Http/Requests/StripeRegistrationFormRequest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Plan;
+use Illuminate\Foundation\Http\FormRequest;
+use Stripe\Customer;
+
+/**
+ * Class StripeRegistrationFormRequest
+ *
+ * @package App\Http\Requests
+ */
+class StripeRegistrationFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'stripeEmail' => 'required|email',
+            'stripeToken' => 'required',
+            'plan' => 'required|in:monthly,yearly'
+        ];
+    }
+
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function save()
+    {
+        $plan = Plan::findOrFail($this->plan);
+
+        $customer = Customer::create(array(
+            'email' => $this->stripeEmail,
+            'source'  => $this->stripeToken,
+            'plan' => $plan->name,
+        ));
+
+        $this->user()->activateStripe($customer->id);
+    }
+}
