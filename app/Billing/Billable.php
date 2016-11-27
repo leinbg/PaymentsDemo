65f3e1c81ec79d0370e7577eebd1a5140efeaa4d
leinbg <leiw4github@gmail.com>
2016-11-27 20:14:43 +0100

    move Billing Funktionen in Billable Trait

---
+++ app/Billing/Billable.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Billable
+ */
+trait Billable
+{
+    /**
+     * @param $customerId
+     *
+     * @return bool
+     */
+    public function activateStripe($customerId, $subscriptionId)
+    {
+        return $this->update([
+            'stripe_id' => $customerId,
+            'stripe_active' => true,
+            'stripe_subscription_id' => $subscriptionId,
+            'stripe_subscription_end_at' => null,
+        ]);
+    }
+
+    public function reactivateStripe()
+    {
+        return $this->update([
+            'stripe_active' => true,
+            'stripe_subscription_end_at' => null, 
+        ]);
+    }
+
+    public function deactivateStripe()
+    {
+        return $this->update([
+            'stripe_active' => false,
+            'stripe_subscription_end_at' => Carbon::now(),
+        ]);
+    }
+
+    /**
+     * get subscription class for the user
+     */
+    public function subscription()
+    {
+        return new Subscription($this);
+    }
+
+    public function isSubscript()
+    {
+        return !! $this->stripe_active && $this->stripe_subscription_end_at == NULL;
+    }
+}
