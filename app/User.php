94751a29bf2572149128118e6b24a83530e5eada
leinbg <leiw4github@gmail.com>
2016-11-27 10:51:34 +0100

    set guarded in user class to empty array

--- app/User.php
+++ app/User.php
@@ -15,14 +15,7 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password', 'stripe_id', 'stripe_active', 'stripe_subscription_end_at'
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for arrays.
