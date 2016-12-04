5c2c805073eace7dcef305d84379030d06f8ea84
leinbg <leiw4github@gmail.com>
2016-12-04 11:13:06 +0100

    display payment history in home panel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,24 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Plans</div>
 
                 <div class="panel-body" id="app">
                     <checkout-form :plans="{{ $plans }}"></checkout-form>
+                </div>
+            </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading">Payment</div>
+
+                <div class="panel-body">
+                    <ul class="list-group">
+                        @foreach(auth()->user()->payments as $payment)
+                            <li class="list-group-item">
+                                {{ $payment->created_at->diffForHumans() }} : <strong>${{ number_format($payment->amount / 100, 2)}}</strong>
+                            </li>
+                        @endforeach
+                    </ul>
                 </div>
             </div>
         </div>
