563e1b5cecbd7d0fa7c8516cf3f6d8446b696d7d
leinbg <leiw4github@gmail.com>
2016-12-04 10:00:15 +0100

    delete billable trait, it broke unittest

--- app/User.php
+++ app/User.php
@@ -25,4 +25,48 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+      /**
+     * @param $customerId
+     *
+     * @return bool
+     */
+    public function activateStripe($customerId, $subscriptionId)
+    {
+        return $this->update([
+            'stripe_id' => $customerId,
+            'stripe_active' => true,
+            'stripe_subscription_id' => $subscriptionId,
+            'stripe_subscription_end_at' => null,
+        ]);
+    }
+
+    public function reactivateStripe()
+    {
+        return $this->update([
+            'stripe_active' => true,
+            'stripe_subscription_end_at' => null, 
+        ]);
+    }
+
+    public function deactivateStripe()
+    {
+        return $this->update([
+            'stripe_active' => false,
+            'stripe_subscription_end_at' => Carbon::now(),
+        ]);
+    }
+
+    /**
+     * get subscription class for the user
+     */
+    public function subscription()
+    {
+        return new Subscription($this);
+    }
+
+    public function isSubscript()
+    {
+        return !! $this->stripe_active && $this->stripe_subscription_end_at == NULL;
+    }
 }
